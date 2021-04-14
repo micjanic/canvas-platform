713e0ec2a08b2850e393020e9a80089823e72120
unknown <micjanic20@gmail.com>
2021-04-13 21:26:34 -0500

    setup with webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+const mode =
+    process.env.NODE_ENV === 'production' ? 'production' : 'development'
+
+module.exports = {
+    mode: mode,
+    entry: './src/index.js',
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    //without additional settings, this will reference .babelrc
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+
+    devtool: 'source-map',
+    devServer: {
+        contentBase: './dist',
+        watchContentBase: true,
+    },
+}
